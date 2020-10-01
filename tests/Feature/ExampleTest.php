28a841d5ac4d4b4ab938469c1fe21269f1ae002d
edellion <edellion1@hotmail.com>
2020-10-01 20:27:49 +0300

    NOTES-1: sorry about tests. Not enough time

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,6 +16,6 @@ class ExampleTest extends TestCase
 //    {
 //        $response = $this->get('/');
 //
-//        $response->assertRedirect('/login');
+//        $response->assertStatus(200);
 //    }
 }
